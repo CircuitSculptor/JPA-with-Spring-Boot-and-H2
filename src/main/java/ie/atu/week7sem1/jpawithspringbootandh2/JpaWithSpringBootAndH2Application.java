e8c61c3fc632ba0daa78f6aab443fb01e056aaa4
Bartlomiej <g00428194@atu.ie>
2025-11-04 20:37:39 +0000

    Created Repository

---
+++ src/main/java/ie/atu/week7sem1/jpawithspringbootandh2/JpaWithSpringBootAndH2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week7sem1.jpawithspringbootandh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWithSpringBootAndH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaWithSpringBootAndH2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week7sem1/jpawithspringbootandh2/JpaWithSpringBootAndH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week7sem1.jpawithspringbootandh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWithSpringBootAndH2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
